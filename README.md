535835cac43d54dfdf258e3b9feefc996c5d240a
James Balamuta <balamut2@illinois.edu>
2018-10-30 21:01:59 -0500

    R 3.5.1 Installer Scripts

---
+++ README.md
@@ -0,0 +1,7 @@
+# R Package Installer Deconstruction
+
+The goal behind this project is to see how the underlying R installer package is created.
+We do this by reverse engineering the installer package using the `pkgutil` tool. 
+Alternative, you can use "Show Package Contents" on the right click context menu. 
+
+
---
+++ deconstruct_pkg.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+#
+# deconstruct_pkg.sh
+#
+# Copyright (C) 2018 James Joseph Balamuta <balamut2@illinois.edu>
+# 
+# Version 1.0 -- 10/30/18
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+
+################################################################
+
+#1 Base URL (with trailing /)
+#2 File name
+download_file(){
+	if [ -f "$2" ]; then
+		echo "$2 has already been downloaded..."
+	else 
+		echo "Downloading file $2 ... Please be patient..."	
+		# Download file into working directory
+		curl -O $1$2
+	fi
+}
+
+################################################################
+
+# Variables
+LIBS_URL="https://cran.r-project.org/bin/macosx/"
+R_INSTALLER="R-3.5.1.pkg"
+
+# Download to the working directory
+download_file $LIBS_URL $R_INSTALLER
+
+# Split apart the downloaded package
+pkgutil --expand $R_INSTALLER r-pkg
+
+# View information
+tree r-pkg
+
+# Create directory to store package scripts
+mkdir r-pkg-scripts
+
+# Copy out the R component scripts
+cp r-pkg/r.pkg/Scripts/* r-pkg-scripts
+
+
